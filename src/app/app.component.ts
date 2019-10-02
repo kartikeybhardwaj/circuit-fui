cabf2141109529b256122550a9844e1d800ba706
Kartikey Bhardwaj <kartikey.bh21@gmail.com>
2019-10-02 19:07:46 +0530

    get user and navigate to required page

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,7 +1,13 @@
 import {
-  Component, OnInit
+  Component,
+  OnInit
 } from '@angular/core';
-import { Router } from '@angular/router';
+import {
+  Router
+} from '@angular/router';
+import {
+  AppStorageService
+} from './app.service';
 
 @Component({
   selector: 'app-root',
@@ -11,11 +17,21 @@ import { Router } from '@angular/router';
 export class AppComponent implements OnInit {
 
   constructor(
+    public appInfo: AppStorageService,
     private router: Router
   ) {}
 
   ngOnInit() {
-    this.router.navigate(['page-not-found']);
+    this.getUser();
+  }
+
+  getUser() {
+    // get user data and set to appInfo.user
+    this.appInfo.user = {
+      username: 'kartoon',
+      displayname: 'kartoon'
+    };
+    this.router.navigate(['home']);
   }
 
 }
