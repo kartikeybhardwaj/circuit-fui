17bf67d5802c5620ef78db8356654139b52477bb
Kartikey Bhardwaj <kartikey.bh21@gmail.com>
2019-10-15 10:33:29 +0530

    added pulses

---
+++ src/app/pulses/pulses.component.ts
@@ -0,0 +1,40 @@
+import {
+  Component,
+  OnInit
+} from '@angular/core';
+import {
+  AppStorageService
+} from '../app.service';
+import {
+  ActivatedRoute,
+  Router
+} from '@angular/router';
+
+@Component({
+  selector: 'app-pulses',
+  templateUrl: './pulses.component.html',
+  styleUrls: ['./pulses.component.css']
+})
+export class PulsesComponent implements OnInit {
+
+  constructor(
+    public appInfo: AppStorageService,
+    private activatedRoute: ActivatedRoute,
+    private router: Router
+  ) {
+    const activatedRouteSnapshot = activatedRoute.snapshot;
+    if (activatedRouteSnapshot.params &&
+      activatedRouteSnapshot.params.projectId &&
+      activatedRouteSnapshot.params.milestoneId) {
+      appInfo.selectedProjectId = activatedRouteSnapshot.params.projectId;
+      appInfo.selectedMilestoneId = activatedRouteSnapshot.params.milestoneId;
+    }
+    appInfo.selectedPulseId = null;
+    appInfo.otherHeader = '';
+    appInfo.navigationAddText = 'Add Pulse';
+    appInfo.isNavigationAddTextVisible = true;
+  }
+
+  ngOnInit() {}
+
+}
