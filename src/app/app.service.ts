ffab52a337cfc5d22222cdb024c686806c0c3c0c
Kartikey Bhardwaj <kartikey.bh21@gmail.com>
2019-10-18 17:19:51 +0530

    new design and bug fixes

--- src/app/app.service.ts
+++ src/app/app.service.ts
@@ -1,7 +1,9 @@
 import {
   Injectable
 } from '@angular/core';
-import { HttpHeaders } from '@angular/common/http';
+import {
+  HttpHeaders
+} from '@angular/common/http';
 
 @Injectable()
 export class AppStorageService {
@@ -19,162 +21,50 @@ export class AppStorageService {
   };
   httpOptionsWithAuth: any = null;
 
-  projects: any = [{
-    name: 'This is public project 1',
-    // tslint:disable-next-line: max-line-length
-    description: 'This is just a description, nothing to read. Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.',
-    visibility: 'public',
-    visibilityIcon: 'public'
-  }, {
-    name: 'This is internal project 2',
-    // tslint:disable-next-line: max-line-length
-    description: 'This is just a description, nothing to read. Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.',
-    visibility: 'internal',
-    visibilityIcon: 'enhanced_encryption'
-  }, {
-    name: 'This is private project 3 and this has a very long name as compared to others',
-    // tslint:disable-next-line: max-line-length
-    description: 'This is just a description, nothing to read. Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.',
-    visibility: 'private',
-    visibilityIcon: 'security'
-  }, {
-    name: 'This is public project 4',
-    // tslint:disable-next-line: max-line-length
-    description: 'This is just a description, nothing to read. Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.',
-    visibility: 'public',
-    visibilityIcon: 'public'
-  }, {
-    name: 'This is internal project 5',
-    // tslint:disable-next-line: max-line-length
-    description: 'This is just a description, nothing to read. Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.',
-    visibility: 'internal',
-    visibilityIcon: 'enhanced_encryption'
-  }, {
-    name: 'This is private project 6',
-    // tslint:disable-next-line: max-line-length
-    description: 'This is just a description, nothing to read. Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.',
-    visibility: 'private',
-    visibilityIcon: 'security'
-  }, {
-    name: 'This is public project 7',
-    // tslint:disable-next-line: max-line-length
-    description: 'This is just a description, nothing to read. Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.',
-    visibility: 'public',
-    visibilityIcon: 'public'
-  }, {
-    name: 'This is internal project 8',
-    // tslint:disable-next-line: max-line-length
-    description: 'This is just a description, nothing to read. Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.',
-    visibility: 'internal',
-    visibilityIcon: 'enhanced_encryption'
-  }, {
-    name: 'This is private project 9',
-    // tslint:disable-next-line: max-line-length
-    description: 'This is just a description, nothing to read. Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.',
-    visibility: 'private',
-    visibilityIcon: 'security'
-  }, {
-    name: 'This is public project 10',
-    // tslint:disable-next-line: max-line-length
-    description: 'This is just a description, nothing to read. Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.',
-    visibility: 'public',
-    visibilityIcon: 'public'
-  }, {
-    name: 'This is internal project 11',
-    // tslint:disable-next-line: max-line-length
-    description: 'This is just a description, nothing to read. Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.',
-    visibility: 'internal',
-    visibilityIcon: 'enhanced_encryption'
-  }, {
-    name: 'This is private project 12',
-    // tslint:disable-next-line: max-line-length
-    description: 'This is just a description, nothing to read. Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.',
-    visibility: 'private',
-    visibilityIcon: 'security'
-  }];
+  projects: any = null;
+  milestones: any = null;
+  pulses: any = null;
 
-  milestones: any = [{
-    name: 'This is a milestone 1',
-    // tslint:disable-next-line: max-line-length
-    description: 'This is just a description, nothing to read. Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.',
-    timeline: {
-      from: 'Tue, Oct 01, \'19',
-      to: 'Tue, Oct 22, \'19'
-    }
+  roles = [{
+    _id: '1',
+    title: 'Super User',
+    description: 'Super User who can do anything.',
+    isSuperUser: true,
+    canModifyUsersRole: true,
+    canModifyLocations: true,
+    canModifyProjects: true,
+    canModifyMilestones: true,
+    canModifyPulses: true
   }, {
-    name: 'This is another milestone 2',
-    // tslint:disable-next-line: max-line-length
-    description: 'This is just a description, nothing to read. Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.',
-    timeline: {
-      from: 'Wed, Oct 02, \'19',
-      to: 'Mon, Oct 21, \'19'
-    }
+    _id: '2',
+    title: 'Project Manager',
+    description: 'Just some designation',
+    isSuperUser: false,
+    canModifyUsersRole: true,
+    canModifyLocations: true,
+    canModifyProjects: true,
+    canModifyMilestones: true,
+    canModifyPulses: true
   }, {
-    name: 'This is yet just another milestone 3',
-    // tslint:disable-next-line: max-line-length
-    description: 'This is just a description, nothing to read. Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.',
-    timeline: {
-      from: 'Thu, Oct 03, \'19',
-      to: 'Sun, Oct 20, \'19'
-    }
+    _id: '3',
+    title: 'Project Member',
+    description: 'Just some designation',
+    isSuperUser: false,
+    canModifyUsersRole: false,
+    canModifyLocations: true,
+    canModifyProjects: false,
+    canModifyMilestones: false,
+    canModifyPulses: true
   }, {
-    name: 'No more milestones 4',
-    // tslint:disable-next-line: max-line-length
-    description: 'This is just a description, nothing to read. Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.',
-    timeline: {
-      from: 'Fri, Oct 04, \'19',
-      to: 'Sat, Oct 19, \'19'
-    }
-  }, {
-    name: 'Enough milestones 5',
-    // tslint:disable-next-line: max-line-length
-    description: 'This is just a description, nothing to read. Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.',
-    timeline: {
-      from: 'Sat, Oct 05, \'19',
-      to: 'Fri, Oct 18, \'19'
-    }
-  }];
-
-  pulses: any = [{
-    name: 'This is a pulse 1',
-    // tslint:disable-next-line: max-line-length
-    description: 'This is just a description, nothing to read. Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.',
-    timeline: {
-      from: 'Tue, Oct 01, \'19',
-      to: 'Tue, Oct 22, \'19'
-    }
-  }, {
-    name: 'This is another pulse 2',
-    // tslint:disable-next-line: max-line-length
-    description: 'This is just a description, nothing to read. Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.',
-    timeline: {
-      from: 'Wed, Oct 02, \'19',
-      to: 'Mon, Oct 21, \'19'
-    }
-  }, {
-    name: 'This is yet just another pulse 3',
-    // tslint:disable-next-line: max-line-length
-    description: 'This is just a description, nothing to read. Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.',
-    timeline: {
-      from: 'Thu, Oct 03, \'19',
-      to: 'Sun, Oct 20, \'19'
-    }
-  }, {
-    name: 'No more pulses 4',
-    // tslint:disable-next-line: max-line-length
-    description: 'This is just a description, nothing to read. Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.',
-    timeline: {
-      from: 'Fri, Oct 04, \'19',
-      to: 'Sat, Oct 19, \'19'
-    }
-  }, {
-    name: 'Enough pulses 5',
-    // tslint:disable-next-line: max-line-length
-    description: 'This is just a description, nothing to read. Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.',
-    timeline: {
-      from: 'Sat, Oct 05, \'19',
-      to: 'Fri, Oct 18, \'19'
-    }
+    _id: '4',
+    title: 'That one guy',
+    description: 'Just some designation',
+    isSuperUser: false,
+    canModifyUsersRole: false,
+    canModifyLocations: true,
+    canModifyProjects: false,
+    canModifyMilestones: false,
+    canModifyPulses: false
   }];
 
   otherHeader: any = '';
@@ -189,6 +79,11 @@ export class AppStorageService {
   getShortDate(date: number): string {
     const thisDate = new Date(date).toString();
     return thisDate.substr(0, 3) + ',' + thisDate.substr(3, 7);
+  }
+
+  getLongDate(date: number): string {
+    const thisDate = new Date(date).toString();
+    return thisDate.substr(0, 3) + ',' + thisDate.substr(3, 7) + ',' + thisDate.substr(10, 5);
   }
 
 }
