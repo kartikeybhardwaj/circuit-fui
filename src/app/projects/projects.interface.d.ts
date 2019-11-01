0fc971ba0395effe124babd669f23d3d505ac649
Kartikey Bhardwaj <kartikey.bh21@gmail.com>
2019-11-02 02:55:37 +0530

    added project interface

---
+++ src/app/projects/projects.interface.d.ts
@@ -0,0 +1,31 @@
+declare interface ProjectData {
+  projectId: string;
+  index: number;
+  title: string;
+  description: string;
+  visibility: string;
+  visibilityIcon: string;
+  members: ProjectMemberData[];
+  milestonesList: string[];
+  milestonesListCount: number;
+  projectMetaId: string;
+  fields: ProjectFieldData[];
+  meta: ProjectMetaData;
+}
+
+declare interface ProjectMemberData {
+  userId: string;
+  roleId: string;
+}
+
+declare interface ProjectFieldData {
+  key: string;
+  value: string;
+}
+
+declare interface ProjectMetaData {
+  addedBy: string;
+  addedOn: string;
+  lastUpdatedBy: string;
+  lastUpdatedOn: string;
+}
