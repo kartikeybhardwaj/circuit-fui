1d44f9ab87ebe6bd5c0f47e94f34a810e471602e
Kartikey Bhardwaj <kartikey.bh21@gmail.com>
2019-11-02 19:46:26 +0530

    getMilestones get req with params

--- src/app/milestones/milestones.service.ts
+++ src/app/milestones/milestones.service.ts
@@ -4,86 +4,68 @@ import {
 import {
   AppStorageService
 } from '../app.service';
+import {
+  HttpClient
+} from '@angular/common/http';
 
 @Injectable()
 export class MilestoneStorageService {
 
+  milestones: MilestoneData[] = [];
+  idMapMilestones: any = {};
+
   constructor(
-    private appInfo: AppStorageService
+    private appInfo: AppStorageService,
+    private http: HttpClient
   ) {}
 
-  getMilestones(): any {
+  getMilestones(projectId: string): any {
     return new Promise((resolve, reject) => {
-      this.appInfo.milestones = [{
-        index: 1,
-        _id: '1234567890',
-        title: 'this is some title',
-        description: 'this is some description',
-        timeline: {
-          begin: this.appInfo.getShortDate(new Date().getTime()),
-          end: this.appInfo.getShortDate(new Date().getTime())
+      const httpOptions = this.appInfo.httpOptions;
+      httpOptions.params = {
+        projectId
+      };
+      this.http.get(this.appInfo.constants.urls.getMilestones, httpOptions).subscribe(
+        (response: any) => {
+          if (response.responseId && response.responseId === 211) {
+            this.milestones = [];
+            this.idMapMilestones = response.data.idMap;
+            response.data.milestones.forEach(milestone => {
+              const pulsesList: string[] = [];
+              milestone.pulsesList.forEach(pulse => {
+                pulsesList.push(pulse);
+              });
+              this.milestones.push({
+                milestoneId: milestone._id,
+                index: milestone.index,
+                title: milestone.title,
+                description: milestone.description,
+                timeline: milestone.timeline,
+                pulsesList,
+                pulsesListCount: milestone.pulsesList.length,
+                milestoneMetaId: milestone.milestoneMetaId,
+                fields: milestone.fields,
+                linkedProjectId: milestone.linkedProjectId,
+                meta: {
+                  addedBy: milestone.meta.addedBy,
+                  addedOn: milestone.meta.addedOn,
+                  lastUpdatedBy: milestone.meta.lastUpdatedBy ? milestone.meta.lastUpdatedBy : null,
+                  lastUpdatedOn: milestone.meta.lastUpdatedOn ? milestone.meta.lastUpdatedOn : null
+                }
+              });
+            });
+            resolve(this.milestones);
+          } else {
+            if (response.message) {
+              reject(response.message);
+            } else {
+              reject(this.appInfo.constants.messages.someErrorOccurred);
+            }
+          }
         },
-        pulsesList: [{
-          _id: '1234567890',
-          title: 'pulse title 1'
-        }, {
-          _id: '1234567890',
-          title: 'pulse title 2'
-        }],
-        pulsesListCount: 2,
-        milestoneMetaId: 'string',
-        fields: [{
-          key: 'key_1',
-          key_1: 'value_1'
-        }, {
-          key: 'key_2',
-          key_2: 'value_2'
-        }, {
-          key: 'key_3',
-          key_3: 'value_3'
-        }],
-        meta: {
-          addedBy: 'some user',
-          addedOn: this.appInfo.getLongDate(new Date().getTime()),
-          lastUpdatedBy: 'some user',
-          lastUpdatedOn: this.appInfo.getLongDate(new Date().getTime())
-        }
-      }, {
-        index: 2,
-        _id: '1234567890',
-        title: 'this is some random title',
-        description: 'this is some description',
-        timeline: {
-          begin: this.appInfo.getShortDate(new Date().getTime()),
-          end: this.appInfo.getShortDate(new Date().getTime())
-        },
-        pulsesList: [{
-          _id: '1234567890',
-          title: 'pulse title 1'
-        }, {
-          _id: '1234567890',
-          title: 'pulse title 2'
-        }],
-        pulsesListCount: 2,
-        milestoneMetaId: 'string',
-        fields: [{
-          key: 'key_1',
-          key_1: 'value_1'
-        }, {
-          key: 'key_2',
-          key_2: 'value_2'
-        }, {
-          key: 'key_3',
-          key_3: 'value_3'
-        }],
-        meta: {
-          addedBy: 'some user',
-          addedOn: this.appInfo.getLongDate(new Date().getTime()),
-          lastUpdatedBy: 'some user',
-          lastUpdatedOn: this.appInfo.getLongDate(new Date().getTime())
-        }
-      }];
-      resolve(this.appInfo.milestones);
+        (error: any) => {
+          reject(error);
+        });
     });
   }
 
