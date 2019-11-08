475b49ac2a8eef97d1fadee450147544fbe2d458
Kartikey Bhardwaj <kartikey.bh21@gmail.com>
2019-11-08 17:26:12 +0530

    added add-pulse service

---
+++ src/app/add-pulse/add-pulse.service.ts
@@ -0,0 +1,45 @@
+import {
+  Injectable
+} from '@angular/core';
+import {
+  AppStorageService
+} from '../app.service';
+import {
+  HttpClient
+} from '@angular/common/http';
+import {
+  AddPulsePayloadValidator
+} from '../json-schema-validatior/add-pulse';
+
+@Injectable()
+export class AddPulseStorageService {
+
+  constructor(
+    private appInfo: AppStorageService,
+    private addPulsePayloadValidator: AddPulsePayloadValidator,
+    private http: HttpClient
+  ) {}
+
+  validateRequest(pulse: AddPulseData): [boolean, string] {
+    return this.addPulsePayloadValidator.validateSchema(pulse);
+  }
+
+  addPulse(reqPayload: AddPulseData): any {
+    return new Promise((resolve, reject) => {
+      this.http.post(this.appInfo.constants.urls.addPulse, JSON.stringify(reqPayload), this.appInfo.httpOptions).subscribe(
+        (response: any) => {
+          if (response.responseId && response.responseId === 211) {
+            resolve([true, 'Pulse added', response.data]);
+          } else if (response.message) {
+            reject([false, response.message, {}]);
+          } else {
+            reject([false, 'Some error occurred', {}]);
+          }
+        },
+        (error: any) => {
+          reject([false, 'Some error occurred', {}]);
+        });
+    });
+  }
+
+}
