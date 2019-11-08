8fe0eee70c181c55465850c496b3eccd5f169ad3
Kartikey Bhardwaj <kartikey.bh21@gmail.com>
2019-11-08 10:38:25 +0530

    added add-project service

---
+++ src/app/add-project/add-project.service.ts
@@ -0,0 +1,45 @@
+import {
+  Injectable
+} from '@angular/core';
+import {
+  AppStorageService
+} from '../app.service';
+import {
+  HttpClient
+} from '@angular/common/http';
+import {
+  AddProjectPayloadValidator
+} from '../json-schema-validatior/add-project';
+
+@Injectable()
+export class AddProjectStorageService {
+
+  constructor(
+    private appInfo: AppStorageService,
+    private addProjectPayloadValidator: AddProjectPayloadValidator,
+    private http: HttpClient
+  ) {}
+
+  validateRequest(project: AddProjectData): [boolean, string] {
+    return this.addProjectPayloadValidator.validateSchema(project);
+  }
+
+  addProject(reqPayload: AddProjectData): any {
+    return new Promise((resolve, reject) => {
+      this.http.post(this.appInfo.constants.urls.addProject, JSON.stringify(reqPayload), this.appInfo.httpOptions).subscribe(
+        (response: any) => {
+          if (response.responseId && response.responseId === 211) {
+            resolve([true, 'Project added', response.data]);
+          } else if (response.message) {
+            reject([false, response.message, {}]);
+          } else {
+            reject([false, 'Some error occurred', {}]);
+          }
+        },
+        (error: any) => {
+          reject([false, 'Some error occurred', {}]);
+        });
+    });
+  }
+
+}
