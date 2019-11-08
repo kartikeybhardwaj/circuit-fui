44c1f13884006c50ff2abe89363dddda2dc55278
Kartikey Bhardwaj <kartikey.bh21@gmail.com>
2019-11-08 17:23:45 +0530

    added meta milestone component

---
+++ src/app/meta-milestones/meta-milestones.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-meta-milestones',
+  templateUrl: './meta-milestones.component.html',
+  styleUrls: ['./meta-milestones.component.css']
+})
+export class MetaMilestonesComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
