2df3fe4f1d838d54c15f2d48d1b1e8e52a42f3d3
Kartikey Bhardwaj <kartikey.bh21@gmail.com>
2019-11-08 17:23:56 +0530

    added meta pulse component

---
+++ src/app/meta-pulses/meta-pulses.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-meta-pulses',
+  templateUrl: './meta-pulses.component.html',
+  styleUrls: ['./meta-pulses.component.css']
+})
+export class MetaPulsesComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
