553ba1252e30db6a1d3662f3368297563cf6d69b
Kartikey Bhardwaj <kartikey.bh21@gmail.com>
2019-11-09 23:50:01 +0530

    added add meta pulse service

---
+++ src/app/add-meta-pulse/add-meta-pulse.service.ts
@@ -0,0 +1,45 @@
+import {
+  Injectable
+} from '@angular/core';
+import {
+  AppStorageService
+} from '../app.service';
+import {
+  HttpClient
+} from '@angular/common/http';
+import {
+  AddMetaPulsePayloadValidator
+} from '../json-schema-validatior/add-meta-pulse';
+
+@Injectable()
+export class AddMetaPulseStorageService {
+
+  constructor(
+    private appInfo: AppStorageService,
+    private addMetaPulsePayloadValidator: AddMetaPulsePayloadValidator,
+    private http: HttpClient
+  ) {}
+
+  validateRequest(metaPulse: AddMetaPulseData): [boolean, string] {
+    return this.addMetaPulsePayloadValidator.validateSchema(metaPulse);
+  }
+
+  addMetaPulse(reqPayload: AddMetaPulseData): any {
+    return new Promise((resolve, reject) => {
+      this.http.post(this.appInfo.constants.urls.addMetaPulse, JSON.stringify(reqPayload), this.appInfo.httpOptions).subscribe(
+        (response: any) => {
+          if (response.responseId && response.responseId === 211) {
+            resolve([true, 'Meta pulse added', response.data]);
+          } else if (response.message) {
+            reject([false, response.message, {}]);
+          } else {
+            reject([false, 'Some error occurred', {}]);
+          }
+        },
+        (error: any) => {
+          reject([false, 'Some error occurred', {}]);
+        });
+    });
+  }
+
+}
