6b377de78c40d2eb0ca8bb1fe5d8a34ee77aafb8
Kartikey Bhardwaj <kartikey.bh21@gmail.com>
2019-11-10 23:04:46 +0530

    calendar fixes

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -3,15 +3,11 @@ import {
   OnInit
 } from '@angular/core';
 import {
-  EventInput
-} from '@fullcalendar/core';
-import dayGridPlugin from '@fullcalendar/daygrid';
-import timeGridPlugin from '@fullcalendar/timegrid';
-import listPlugin from '@fullcalendar/list';
-import interactionPlugin from '@fullcalendar/interaction';
-import {
   AppStorageService
 } from '../app.service';
+import {
+  HomeStorageService
+} from './home.service';
 
 @Component({
   selector: 'app-home',
@@ -20,114 +16,9 @@ import {
 })
 export class HomeComponent implements OnInit {
 
-  calendarPlugins = [dayGridPlugin, timeGridPlugin, listPlugin, interactionPlugin];
-  calendarHeader = {
-    left: 'prev,next today',
-    center: 'title',
-    right: 'dayGridMonth,timeGridWeek,timeGridDay,listWeek'
-  };
-  calendarBusinessHours = {
-    // days of week. an array of zero-based day of week integers (0=Sunday)
-    daysOfWeek: [1, 2, 3, 4, 5], // Monday - Friday
-    startTime: '09:15',
-    endTime: '17:30',
-  };
-  calendarWeekends = true;
-  calendarWeekNumbers = true;
-  calendarSelectable = true;
-  calendarNowIndicator = true;
-  calendarEventLimit = true;
-  calendarEditable = true;
-  calendarEvents: EventInput[] = [{
-    _id: '1234567890',
-    title: 'Some event name',
-    url: 'https://www.google.com/',
-    start: new Date(),
-    end: new Date(),
-    allDay: true,
-    color: 'purple'
-  }, {
-    _id: '1234567890',
-    title: 'Another event',
-    url: 'https://www.google.com/',
-    start: new Date('2019-10-30'),
-    end: new Date('2019-10-30'),
-    allDay: true
-  }, {
-    _id: '1234567890',
-    title: 'Party event',
-    url: 'https://www.google.com/',
-    start: new Date('2019-10-09'),
-    end: new Date('2019-10-09'),
-    allDay: true,
-    color: 'purple'
-  }, {
-    _id: '1234567890',
-    title: 'Random event',
-    url: 'https://www.google.com/',
-    start: new Date('2019-10-20'),
-    end: new Date('2019-10-22'),
-    allDay: true
-  }, {
-    _id: '1234567890',
-    title: 'Random event',
-    url: 'https://www.google.com/',
-    start: new Date('2019-10-20'),
-    end: new Date('2019-10-22'),
-    allDay: true
-  }, {
-    _id: '1234567890',
-    title: 'Random event',
-    url: 'https://www.google.com/',
-    start: new Date('2019-10-20'),
-    end: new Date('2019-10-22'),
-    allDay: true
-  }, {
-    _id: '1234567890',
-    title: 'Random event',
-    url: 'https://www.google.com/',
-    start: new Date('2019-10-20'),
-    end: new Date('2019-10-22'),
-    allDay: true
-  }, {
-    _id: '1234567890',
-    title: 'Random event',
-    url: 'https://www.google.com/',
-    start: new Date('2019-10-20'),
-    end: new Date('2019-10-22'),
-    allDay: true
-  }, {
-    _id: '1234567890',
-    title: 'Random event',
-    url: 'https://www.google.com/',
-    start: new Date('2019-10-20'),
-    end: new Date('2019-10-22'),
-    allDay: true
-  }, {
-    _id: '1234567890',
-    title: 'Random event',
-    url: 'https://www.google.com/',
-    start: new Date('2019-10-20'),
-    end: new Date('2019-10-22'),
-    allDay: true
-  }, {
-    _id: '1234567890',
-    title: 'Random event',
-    url: 'https://www.google.com/',
-    start: new Date('2019-10-20'),
-    end: new Date('2019-10-22'),
-    allDay: true
-  }, {
-    _id: '1234567890',
-    title: 'Random event',
-    url: 'https://www.google.com/',
-    start: new Date('2019-10-20'),
-    end: new Date('2019-10-22'),
-    allDay: true
-  }];
-
   constructor(
-    public appInfo: AppStorageService
+    public appInfo: AppStorageService,
+    public homeInfo: HomeStorageService
   ) {
     appInfo.selectedProjectId = null;
     appInfo.selectedMilestoneId = null;
@@ -137,7 +28,9 @@ export class HomeComponent implements OnInit {
     appInfo.isNavigationAddTextVisible = false;
   }
 
-  ngOnInit() {}
+  ngOnInit() {
+    this.homeInfo.getMyPulses();
+  }
 
   pulseClick(info: any): void {
     info.jsEvent.preventDefault(); // don't let the browser navigate
